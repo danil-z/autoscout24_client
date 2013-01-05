72eca3bfc9c032852d6310dca0a58f1de9f7d2b3
blackbumer <danil@autoservie61.ru>
2013-01-05 16:49:51 +0400

    #722313-42000183 Vehicle#name

--- lib/autoscout24_client/vehicle.rb
+++ lib/autoscout24_client/vehicle.rb
@@ -21,6 +21,12 @@ module Autoscout24Client
       @source = node
     end
 
+    # @return [String] model name like C220, A4, Pajero and etc
+    def name
+      brand_node = self.class.make_model_tree.find{|node| node["id"] == source["brand_id"].to_s}
+      text_by_value(source["model_id"].to_s,brand_node)
+    end
+
     ## @return [String] name of gear_type
     def gear_type
       @gear_type ||= generic_finder :gear_types, "gear_type_id"
@@ -36,6 +42,16 @@ module Autoscout24Client
       @fuel ||= generic_finder :fuels, "fuel_type_id"
     end
 
+#    def method_missing(meth, *args, &block)
+    def method_missing(meth)
+      keys = meth.id2name.split("_")
+      keys.count>1 ? nested_hash_values(keys).compact : nested_hash_value(keys[0])
+    end
+
+    private
+
+    #  hash helpers
+
     def generic_finder(collection, primary_key)
       self.class.send(collection).each do |object|
         return object["text"] if object["id"].to_s == source[primary_key].to_s
@@ -43,14 +59,6 @@ module Autoscout24Client
       nil
     end
 
-#    def method_missing(meth, *args, &block)
-    def method_missing(meth)
-      keys = meth.id2name.split("_")
-      keys.count>1 ? nested_hash_values(keys).compact : nested_hash_value(keys[0])
-    end
-
-    #  hash helpers
-    ## TODO: move this to external file
     def nested_hash_value(key, obj = source)
       if obj.respond_to?(:key?) && obj.key?(key)
         obj[key]
@@ -65,6 +73,18 @@ module Autoscout24Client
       normalized_keys = keys.kind_of?(String) ? keys.split(",") :keys
       normalized_keys.map{|k| nested_hash_value(k,obj)}
     end
+
+
+    def text_by_value(value, obj)
+      if obj.respond_to?(:key?) && obj.key?("id") && obj["id"] == value
+        obj["text"]
+      elsif obj.respond_to?(:each)
+        r = nil
+        obj.find{ |*a| r=text_by_value(value, a.last) }
+        r
+      end
+    end
+
 
   end
 end
--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -36,8 +36,6 @@ describe Autoscout24Client::Vehicle do
   end
 
   describe ".method_missing" do
-  #  let(:attrs) { %w(mileage accident_free body_color kilowatt) }
-  #
     it "searches in source method name key" do
       vehicle = Autoscout24Client::Vehicle.new("mykey" => 135)
       vehicle.send("mykey".to_sym).must_equal 135
@@ -47,6 +45,21 @@ describe Autoscout24Client::Vehicle do
       vehicle.send("mykey_zip".to_sym).must_equal [135, "344000"]
     end
   end
-  #
+
+  describe ".name" do
+    let(:mod_tree){
+      [
+          {"name"=>"brand", "id"=>"15", "text"=>"Bugatti", "nodes"=>{"node"=>[{"name"=>"model", "id"=>"15677", "text"=>"EB 110"}, {"name"=>"model", "id"=>"18843", "text"=>"Veyron"}]}},
+          {"name"=>"brand", "id"=>"16337", "text"=>"Galloper", "nodes"=>{"node"=>[{"name"=>"model", "id"=>"16562", "text"=>"GALLOPER"}]}}
+      ]
+    }
+    it "return model name by model_id (a4, lancer, x5 and etc...)" do
+      Autoscout24Client::Vehicle.stub :make_model_tree, mod_tree do
+        vehicle = Autoscout24Client::Vehicle.new("brand_id"=>15, "model_id"=> 18843)
+        vehicle.name.must_equal "Veyron"
+      end
+    end
+  end
+
 
 end
